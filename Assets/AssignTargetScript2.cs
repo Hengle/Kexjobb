040b079f63ac0657e57067fe1a13c2ab9a1a25d4
martin <martin.funkquist@gmail.com>
2016-03-23 15:42:44 +0100

    Fixed group formations.

---
+++ Assets/AssignTargetScript2.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnitySteer.Behaviors;
+public class AssignTargetScript2 : MonoBehaviour {
+	public Transform targetPoint;
+	// Use this for initialization
+	void Start () {
+		foreach(Transform child in transform)
+		{
+			child.GetComponent<SteerToFollow>()._target = targetPoint;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
